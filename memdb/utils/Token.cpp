0dc57134e8ab9e0f749c0242f1524158e4b5ecb3
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-11-21 12:02:26 +0300

    Add Token class method definitions to Token.cpp

--- memdb/utils/Token.cpp
+++ memdb/utils/Token.cpp
@@ -1,6 +1,7 @@
 #include "Token.h"
 
-Token::Token(const Type type, const std::string& value) : type_(type), value_(value) {}
+Token::Token(const Type type, const std::string& value) : type_(type), value_(value) {
+}
 
 Token::Type Token::GetType() const {
     return type_;
@@ -9,5 +10,3 @@ Token::Type Token::GetType() const {
 const std::string& Token::GetValue() const {
     return value_;
 }
-
-
