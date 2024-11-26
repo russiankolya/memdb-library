ef534c6be40a7c3da9d01f70d08031313ef5f682
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-11-26 22:50:41 +0300

    Add new setter

--- memdb/Row.cpp
+++ memdb/Row.cpp
@@ -15,7 +15,6 @@ void Row::SetCells(std::vector<std::unique_ptr<Cell>> &&cells) {
     cells_ = std::move(cells);
 }
 
-
 void Row::AddCell(std::unique_ptr<Cell> &&cell) {
     cells_.push_back(std::move(cell));
 }
