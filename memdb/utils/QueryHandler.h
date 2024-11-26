5285f17680afe9264c7336f5dee5ad4cad8b9de9
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-11-26 22:51:15 +0300

    Add new not finished function

--- memdb/utils/QueryHandler.h
+++ memdb/utils/QueryHandler.h
@@ -15,6 +15,7 @@ public:
     virtual Response Execute(std::map<std::string, std::unique_ptr<Table>> &current_tables) = 0;
 
     static std::vector<uint8_t> HexStringToVector(const std::string& hex_string);
+
 protected:
     const std::vector<Token>& tokens_;
 };
